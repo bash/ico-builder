0d213e7fe1116ccb5c610fbd439e169b1c5a6f65
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-04-18 23:20:50 +0200

    Replace unwrap with expect

--- src/lib.rs
+++ src/lib.rs
@@ -122,7 +122,9 @@ impl IcoBuilder {
     /// ## Panics
     /// This function panics if the path of one of the source files is not valid UTF-8.
     pub fn build_file_cargo(&self, file_name: impl AsRef<OsStr>) -> Result<PathBuf> {
-        let out_dir = env::var("OUT_DIR").unwrap();
+        let out_dir = env::var("OUT_DIR").expect(
+            "OUT_DIR environment variable is required.\nHint: This function is intended to be used in Cargo build scripts.",
+        );
         let mut output_path = PathBuf::from(out_dir);
         output_path.push(file_name.as_ref());
 
